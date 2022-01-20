295958e20a81c7976eb60408da8c752a5a76de1b
RuExe <ru.exe.grizzly@mail.ru>
2022-01-20 22:33:05 +0700

    Исправлено 4 задание

--- 4/main.go
+++ 4/main.go
@@ -8,37 +8,13 @@ import (
 
 func main() {
 	slice := []string{
-		"пятак", "пятка", "тяпка", "листок", "слиток", "столик", "слиток",
+		"пятак", "листок", "слиток", "столик", "слиток", "тяпка", "пятка",
 	}
-	sort.Strings(slice)
 
 	res := Anagrams(&slice)
 	fmt.Println(slice)
 	fmt.Println(*res)
 }
-
-//func Anagrams(slice []string) *map[string][]string {
-//	table := map[string][]string{}
-//	for i := 0; i < len(slice); i++ {
-//		for j := i + 1; j < len(slice); j++ {
-//			iWord := strings.ToLower(slice[i])
-//			jWord := strings.ToLower(slice[j])
-//
-//			iSorted := SortStringByCharacter(iWord)
-//			jSorted := SortStringByCharacter(jWord)
-//			if iSorted == jSorted {
-//				table[iSorted] = append(table[iSorted], jWord)
-//			}
-//		}
-//	}
-//
-//	for k := range table {
-//		if len(table[k]) < 2 {
-//			delete(table, k)
-//		}
-//	}
-//	return &table
-//}
 
 func Anagrams(slice *[]string) *map[string][]string {
 	keys := make(map[string]struct{})
@@ -57,7 +33,9 @@ func Anagrams(slice *[]string) *map[string][]string {
 	res := make(map[string][]string)
 	for k, v := range table {
 		if len(table[k]) > 2 {
-			res[v[0]] = v[1:]
+			temp := v[1:]
+			sort.Strings(temp)
+			res[v[0]] = temp
 		}
 	}
 	return &res
